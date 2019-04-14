0050eb6f9db5292d3a10d12d3a9781e57875cd36
Kevin <1575631865@qq.com>
2019-04-14 14:32:54 +0800

    Add functions

--- app/src/main/java/com/example/focusassistant/FocusOnMeetingActivity.java
+++ app/src/main/java/com/example/focusassistant/FocusOnMeetingActivity.java
@@ -1,11 +1,16 @@
 package com.example.focusassistant;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -55,6 +60,44 @@ public class FocusOnMeetingActivity extends AppCompatActivity {
                                 Toast.makeText(FocusOnMeetingActivity.this, "写下自己的笔记", Toast.LENGTH_SHORT).show();
                             }
                         }).show();
+            }
+        });
+        Button muteoff= findViewById(R.id.muteoff);
+        muteoff.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+                        && !notificationManager.isNotificationPolicyAccessGranted())
+
+                {
+                    Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                    getApplicationContext().startActivity(intent);
+                }
+                AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Toast.makeText(FocusOnMeetingActivity.this, "开启静音", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button muteon=(Button)findViewById(R.id.muteon);
+        muteon.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+                        && !notificationManager.isNotificationPolicyAccessGranted())
+
+                {
+                    Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                    getApplicationContext().startActivity(intent);
+                }
+                AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+//               audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Toast.makeText(FocusOnMeetingActivity.this, "关闭静音", Toast.LENGTH_SHORT).show();
             }
         });
     }
